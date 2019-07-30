2cb1ae006f2281747776c0099cd6cb5dcfee98eb
NakatoTeshika <aleksa_aleksa98@mail.ru>
2019-07-30 09:05:27 +0600

    Рефактор app - добавлено отображение количетсва собак и кошек, сколько поместилось, сколько нет, сколько еще можно поместить для браузера

--- src/HtmlView.php
+++ src/HtmlView.php
@@ -12,15 +12,21 @@ class HtmlView extends View
      */
     public function view(array $arr)
     {
-        echo "Количество животных в коробке " . $arr['animals'] . "<br />";
-        echo "Количество кошек в коробке " . $arr['countCatIn'] . "<br />";
-        echo "Количество собак в коробке " . $arr['countDogIn'] . "<br />";
-        echo "Количество кошек вне коробки " . $arr['countCatOut'] . "<br />";
-        echo "Количество собак вне коробки " . $arr['countDogOut'] . "<br />";
-        echo "Количество сытых в коробке " . $arr['countNotHungryIn'] . "<br />";
-        echo "Количество голодных в коробке " . $arr['countHungryIn'] . "<br />";
-        echo "Количество голодных вне коробки " . $arr['countHungryOut'] . "<br />";
-        echo "Количество сытых вне коробки " . $arr['countNotHungryOut'] . "<br />";
+        echo "Количество животных в коробке "   . $arr['animals']           . "<br />";
+        echo "Количество кошек в коробке "      . $arr['countCatIn']        . "<br />";
+        echo "Количество собак в коробке "      . $arr['countDogIn']        . "<br />";
+        echo "Количество кошек вне коробки "    . $arr['countCatOut']       . "<br />";
+        echo "Количество собак вне коробки "    . $arr['countDogOut']       . "<br />";
+        echo "Количество сытых в коробке "      . $arr['countNotHungryIn']  . "<br />";
+        echo "Количество голодных в коробке "   . $arr['countHungryIn']     . "<br />";
+        echo "Количество голодных вне коробки " . $arr['countHungryOut']    . "<br />";
+        echo "Количество сытых вне коробки "    . $arr['countNotHungryOut'] . "<br />";
+        echo "=================================================================== " . "<br />";
+        echo "Было добавлено собак = " .$arr['addedDog']. " не было добавлено = " . $arr['notAddedDog'] ."<br/>";
+        echo "Было добавлено кошек = " .$arr['addedCat']. " не было добавлено = " . $arr['notAddedCat'] ."<br/>";
+        echo "=================================================================== <br/>";
+        echo (int)$arr['newSpaceCat'] . " столько котиков может поместиться в коробку <br/>";
+        echo (int)$arr['newSpaceDog'] . " столько щеночков может поместиться в коробку <br/>";
         if($arr['clear'] == true) {
             echo "Пора убираться!";
         } else echo "Не надо убираться!";
