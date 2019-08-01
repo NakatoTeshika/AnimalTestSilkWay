63c6073fd67832c8357acee130765d743f27be19
NakatoTeshika <aleksa_aleksa98@mail.ru>
2019-08-01 14:00:33 +0600

    Рефактор в app - строки заменены функциями, которые возвращают строки

--- src/CliView.php
+++ src/CliView.php
@@ -2,35 +2,41 @@
 
 namespace App;
 
+use App\Abstracts\BoxOrNot;
+use App\Abstracts\ParameterParser;
+use App\Abstracts\Presenter;
 use App\Abstracts\View;
+use App\Patterns\BoxPresenter;
+use App\Patterns\OutOfBoxPresenter;
 
 class CliView extends View
 {
     /**
-     * @param array $arr
-     * "View" для cli.php - консоль
+     * @param BoxPresenter $boxPresenter
+     * @param OutOfBoxPresenter $outOfBoxPresenter
+     * Вывод всех сообщений
      */
-    public function view(array $arr)
+    public function view(BoxPresenter $boxPresenter, OutOfBoxPresenter $outOfBoxPresenter)
     {
-        echo "Количество животных в коробке "   . $arr['animals']           . "\n";
-        echo "Количество животных вне коробки " . $arr['animalsOut']        . "\n";
-        echo "Количество кошек в коробке "      . $arr['countCatIn']        . "\n";
-        echo "Количество собак в коробке "      . $arr['countDogIn']        . "\n";
-        echo "Количество кошек вне коробки "    . $arr['countCatOut']       . "\n";
-        echo "Количество собак вне коробки "    . $arr['countDogOut']       . "\n";
-        echo "Количество сытых в коробке "      . $arr['countNotHungryIn']  . "\n";
-        echo "Количество голодных в коробке "   . $arr['countHungryIn']     . "\n";
-        echo "Количество голодных вне коробки " . $arr['countHungryOut']    . "\n";
-        echo "Количество сытых вне коробки "    . $arr['countNotHungryOut'] . "\n";
+        echo $boxPresenter->animalCount() . "\n";
+        echo $outOfBoxPresenter->animalCount()        . "\n";
+        echo $boxPresenter->countCat()        . "\n";
+        echo $boxPresenter->countDog()       . "\n";
+        echo $outOfBoxPresenter->countCat()       . "\n";
+        echo $outOfBoxPresenter->countDog()      . "\n";
+        echo $boxPresenter->countNotHungry()  . "\n";
+        echo $boxPresenter->countHungry()   . "\n";
+        echo $outOfBoxPresenter->countHungry()  . "\n";
+        echo $outOfBoxPresenter->countNotHungry() . "\n";
         echo "=================================================================== \n";
-        echo "Было добавлено собак = " .$arr['addedDog']. " не было добавлено = " . $arr['notAddedDog'] ."\n";
-        echo "Было добавлено кошек = " .$arr['addedCat']. " не было добавлено = " . $arr['notAddedCat'] ."\n";
+        echo $boxPresenter->countIsAddCat() ." ". $outOfBoxPresenter->countIsAddCat() ."\n";
+        echo $boxPresenter->countIsAddDog() ." ". $outOfBoxPresenter->countIsAddDog() ."\n";
         echo "=================================================================== \n";
-        echo (int)$arr['newSpaceCat'] . " столько котиков может поместиться в коробку \n";
-        echo (int)$arr['newSpaceDog'] . " столько щеночков может поместиться в коробку \n";
+        echo $boxPresenter->spaceForCat(). "\n";
+        echo $boxPresenter->spaceForDog() . "\n";
         echo "=================================================================== \n";
-        if($arr['clear'] == true) {
-            echo "Пора убираться!";
-        } else echo "Не надо убираться!";
+        if ($boxPresenter->messageClear()) {
+            echo $boxPresenter->messageClearBox() . "\n";
+        } else echo $boxPresenter->messageNotClear() . "\n";
     }
 }
