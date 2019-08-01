d33a179f53fea4ba6a8773beb9d840d25049ec5e
NakatoTeshika <aleksa_aleksa98@mail.ru>
2019-08-01 13:43:00 +0600

    Рефактор в app - добавлена реализация метода create для коробки

--- src/Patterns/BoxFactory.php
+++ src/Patterns/BoxFactory.php
@@ -1,10 +1,23 @@
 <?php
 
-
 namespace App\Patterns;
 
+use App\Abstracts\ParameterParser;
+use App\Box;
+use App\Interfaces\IFactory;
 
-class BoxFactory
+class BoxFactory implements IFactory
 {
-
+    /**
+     * @param ParameterParser $counter
+     * @return Box|mixed
+     * Создание коробки с заданной пользователем площадью коробки
+     */
+    static public function create(ParameterParser $counter)
+    {
+        $colourOfBox = ["Зеленая","Красная","Черная","Синяя"];
+        $volumeOfBox = $counter->getBoxVolume();
+        $box = new Box($colourOfBox[array_rand($colourOfBox,1)], $volumeOfBox);
+        return $box;
+    }
 }
