c8dd8173e74b784b6b215398c052b06966607f08
NakatoTeshika <aleksa_aleksa98@mail.ru>
2019-08-01 13:52:02 +0600

    Рефактор в app - изменен метод ifAnimal на animalEatIn, животные в коробке отдельно кормятся

--- src/Box.php
+++ src/Box.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use App\Abstracts\Animal;
+use App\Abstracts\ParameterParser;
+use App\Interfaces\ICounter;
 use App\Traits\Waste;
 
-class Box
+class Box implements ICounter
 {
     /**
      * Трейт экскрементов
@@ -14,7 +16,8 @@ class Box
     /**
      * Площадь коробки
      */
-    const SQUARE = 10000;
+//    const SQUARE = 10000;
+    protected $volumeOfBox;
     /**
      * @var Цвет коробки
      */
@@ -27,6 +30,8 @@ class Box
      * @var array Массив животных в коробке
      */
     public $storageOfPet = array();
+
+//    protected $storageOfPetOut = array();
     /**
      * @var int
      * Площадь занятая на текущий момент
@@ -44,34 +49,35 @@ class Box
     {
         return $this->currentSpace;
     }
+
     /**
      * Box constructor.
      * @param $colour
      * Конструктор коробки с одним свойством - цветом коробки
+     * @param $volumeOfBox
      */
-    public function __construct($colour)
+    public function __construct($colour, $volumeOfBox)
     {
         $this->colour = $colour;
+        $this->volumeOfBox = $volumeOfBox;
     }
 
     /**
      * @param Animal $animal
+     * @param ParameterParser $parameter
      * @return bool
      * Добавление животных в коробку,
      * проверяем поместится ли животное в коробку.
      * Устанавливаем флаг inBox и занимаем площадь
      */
-    public function addAnimal(Animal $animal):bool
+    public function addAnimal(Animal $animal, ParameterParser $parameter):bool
     {
-            if (($this->currentSpace + $animal->getVolumeAnimal()) < self::SQUARE) {
-                $animal->setInBox(1);
+            if (($this->currentSpace + $animal->getVolumeAnimal()) < $parameter->getBoxVolume()) {
+                $animal->setInBox(true);
                 array_push($this->storageOfPet, $animal);
                 $this->currentSpace += $animal->getVolumeAnimal();
                 return true;
-            } elseif ($this->currentSpace + $animal->getVolumeAnimal() > self::SQUARE) {
-                $animal->setInBox(0);
-                return false;
-            }
+            } else return false;
     }
 
     /**
@@ -100,21 +106,19 @@ class Box
     }
 
     /**
-     * @param Animal $animal
      * @return bool
-     * Проверка, находится ли животное в коробке
+     * Кормление животных и проверка необходимо ли им в туалет
      */
-    public function ifInBox(Animal $animal):bool
+    public function animalEatIn():bool
     {
-        if ($animal->getInBox()== 1) {
+        foreach ($this->storageOfPet as $animal)
+        {
             $animal->animalEat($animal);
             $animal->ifAnimalToilet();
             $this->total =  $this->totalWaste();
             $this->animalToilet();
             return true;
-        } else {
-            $animal->animalEat($animal);
-            return false;
+
         }
     }
 
@@ -149,6 +153,86 @@ class Box
     public function clearBox(): void
     {
         $this->setWeightOfWaste(0);
+    }
+
+    /**
+     * @return array
+     * Количество голодных и сытых в коробке
+     */
+    public function countIsNotHungry(): array
+    {
+        $countNotHungry = 0;
+        $countHungry    = 0;
+
+        foreach ($this->storageOfPet as $animal) {
+            if ($animal->isHungry($animal) == true && $animal->getInBox()==1) {
+                $countNotHungry++;
+            } elseif ($animal->isHungry($animal) == false && $animal->getInBox()==1) {
+                $countHungry++;
+            }
+        }
+        return ['countNotHungryIn'=>$countNotHungry, 'countHungryIn'=>$countHungry];
+    }
+
+    /**
+     * @return array|mixed
+     * Количество кошек и собак в коробке - не общее число, а по виду животного. Сколько было добавлено
+     */
+    public function countIsAdd()
+    {
+        $addedDog    = 0;
+        $addedCat    = 0;
+
+        foreach ($this->storageOfPet as $animal) {
+            if (is_a($animal, Dog::class)) {
+                $addedDog++;
+            }
+            if(is_a($animal,Cat::class)) {
+                $addedCat++;
+            }
+        }
+        return ['addedDog'=>$addedDog, 'addedCat'=>$addedCat];
+    }
+
+    /**
+     * @param ParameterParser $parameter
+     * @return array
+     * Количество животных помещающихся в коробку
+     */
+    public function enoughPlace(ParameterParser $parameter)
+    {
+        $newSpaceCat = 0;
+        $newSpaceDog = 0;
+
+        if ($parameter->getBoxVolume() - $this->getCurrentSpace() < $parameter->getBoxVolume() && Cat::class) {
+            $newSpaceCat = $parameter->getBoxVolume() - $this->getCurrentSpace();
+            $newSpaceCat = $newSpaceCat / 1000;
+        }
+        if ($parameter->getBoxVolume() - $this->getCurrentSpace() < $parameter->getBoxVolume() && Dog::class) {
+            $newSpaceDog = $parameter->getBoxVolume() - $this->getCurrentSpace();
+            $newSpaceDog = $newSpaceDog / 1100;
+        }
+        return ['newSpaceCat'=>$newSpaceCat, 'newSpaceDog'=>$newSpaceDog];
+    }
+
+    /**
+     * @return array|mixed
+     * Количество животных по виду(кошка,собака) в коробке
+     */
+    public function typeAnimalInOutBox()
+    {
+        $countDogIn = 0;
+        $countCatIn = 0;
+
+        foreach ($this->storageOfPet as $animal) {
+            if (get_class($animal) == Dog::class) {
+                $countDogIn++;
+            } else if (get_class($animal) == Cat::class) {
+                $countCatIn++;
+            }
+
+        }
+        return ['countDogIn'=>$countDogIn,'countCatIn'=>$countCatIn];
     }
 
 }
