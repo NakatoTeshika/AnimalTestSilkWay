eaec6502eab3bf1e0b74bca09f71b43c78a39785
NakatoTeshika <aleksa_aleksa98@mail.ru>
2019-08-01 13:49:53 +0600

    Рефактор в app - объявлены методы для подсчета

--- src/Interfaces/ICounter.php
+++ src/Interfaces/ICounter.php
@@ -1,10 +1,24 @@
 <?php
 
-
 namespace App\Interfaces;
-
 
 interface ICounter
 {
+    /**
+     * @return mixed
+     * Подсчитывает количество добавленных в коробку и не добавленных животных
+     */
+    public function countIsAdd();
 
+    /**
+     * @return mixed
+     * Подсчитывает количество голодных и сытых в коробке и вне коробки
+     */
+    public function countIsNotHungry();
+
+    /**
+     * @return mixed
+     * Подсчитывает количество животных по типу - кошка и собака, в коробке и вне коробки
+     */
+    public function typeAnimalInOutBox();
 }
