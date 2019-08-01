e41b3e53ef762bb5565a7b2d04a1015727825df9
NakatoTeshika <aleksa_aleksa98@mail.ru>
2019-08-01 13:57:48 +0600

    Рефактор в app - заменены входные параметры на Presenter

--- src/Abstracts/View.php
+++ src/Abstracts/View.php
@@ -2,12 +2,16 @@
 
 namespace App\Abstracts;
 
+use App\Patterns\BoxPresenter;
+use App\Patterns\OutOfBoxPresenter;
+
 abstract class View
 {
     /**
-     * @param array $array
+     * @param BoxPresenter $boxPresenter
+     * @param OutOfBoxPresenter $outOfBoxPresenter
      * @return mixed
      * Отображение результатов выполнения функций
      */
-    abstract public function view(array $array);
+    abstract public function view(BoxPresenter $boxPresenter, OutOfBoxPresenter $outOfBoxPresenter);
 }
