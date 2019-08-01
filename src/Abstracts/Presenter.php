d1d82ccbe7ca99f3e9302edae72dbf14505b356f
NakatoTeshika <aleksa_aleksa98@mail.ru>
2019-08-01 13:37:44 +0600

    Рефактор в app - Добавлен абстрактный класс Presenter, в нем методы для подсчета

--- src/Abstracts/Presenter.php
+++ src/Abstracts/Presenter.php
@@ -1,10 +1,48 @@
 <?php
 
-
 namespace App\Abstracts;
 
-
-class Presenter
+abstract class Presenter
 {
+    /**
+     * @return string
+     * Возвращает строку количество животных в коробке и вне коробки
+     */
+    abstract public function animalCount():string;
 
+    /**
+     * @return string
+     * Возвращает строку сколько кошек было добавлено и не добавлено
+     */
+    abstract public function countIsAddCat():string;
+
+    /**
+     * @return string
+     * Возвращает строку сколько щенков было добавлено и не добавлено
+     */
+    abstract public function countIsAddDog():string;
+
+    /**
+     * @return mixed
+     * Возвращает строку с количество кошек в коробке и вне коробки
+     */
+    abstract public function countCat();
+
+    /**
+     * @return mixed
+     * Возвращает количество щенят в коробку и вне коробки
+     */
+    abstract public function countDog();
+
+    /**
+     * @return mixed
+     * Возвращает строку с количеством сытых в коробке и вне коробки
+     */
+    abstract public function countNotHungry();
+
+    /**
+     * @return mixed
+     * Возвращает строку с количеством голодных в коробке и вне коробки
+     */
+    abstract public function countHungry();
 }
