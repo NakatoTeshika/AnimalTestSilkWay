7ea7eee11ffb5a98795801ac3bf395c0e8cd6bc7
NakatoTeshika <aleksa_aleksa98@mail.ru>
2019-08-06 07:11:42 +0600

    Рефактор в HtmlView and CliView  - удалены пробелы между точками

--- src/CliView.php
+++ src/CliView.php
@@ -3,8 +3,6 @@
 namespace App;
 
 use App\Abstracts\BoxOrNot;
-use App\Abstracts\ParameterParser;
-use App\Abstracts\Presenter;
 use App\Abstracts\View;
 use App\Patterns\BoxPresenter;
 use App\Patterns\OutOfBoxPresenter;
@@ -19,24 +17,22 @@ class CliView extends View
     public function view(BoxPresenter $boxPresenter, OutOfBoxPresenter $outOfBoxPresenter)
     {
         echo $boxPresenter->animalCount() . "\n";
-        echo $outOfBoxPresenter->animalCount()        . "\n";
-        echo $boxPresenter->countCat()        . "\n";
-        echo $boxPresenter->countDog()       . "\n";
-        echo $outOfBoxPresenter->countCat()       . "\n";
-        echo $outOfBoxPresenter->countDog()      . "\n";
-        echo $boxPresenter->countNotHungry()  . "\n";
-        echo $boxPresenter->countHungry()   . "\n";
-        echo $outOfBoxPresenter->countHungry()  . "\n";
+        echo $outOfBoxPresenter->animalCount() . "\n";
+        echo $boxPresenter->countCat() . "\n";
+        echo $boxPresenter->countDog() . "\n";
+        echo $outOfBoxPresenter->countCat() . "\n";
+        echo $outOfBoxPresenter->countDog() . "\n";
+        echo $boxPresenter->countNotHungry() . "\n";
+        echo $boxPresenter->countHungry() . "\n";
+        echo $outOfBoxPresenter->countHungry() . "\n";
         echo $outOfBoxPresenter->countNotHungry() . "\n";
         echo "=================================================================== \n";
         echo $boxPresenter->countIsAddCat() ." ". $outOfBoxPresenter->countIsAddCat() ."\n";
         echo $boxPresenter->countIsAddDog() ." ". $outOfBoxPresenter->countIsAddDog() ."\n";
         echo "=================================================================== \n";
-        echo $boxPresenter->spaceForCat(). "\n";
+        echo $boxPresenter->spaceForCat() . "\n";
         echo $boxPresenter->spaceForDog() . "\n";
         echo "=================================================================== \n";
-        if ($boxPresenter->messageClear()) {
-            echo $boxPresenter->messageClearBox() . "\n";
-        } else echo $boxPresenter->messageNotClear() . "\n";
+        echo $boxPresenter->messageClear();
     }
 }
