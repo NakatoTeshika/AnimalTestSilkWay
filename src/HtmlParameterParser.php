658e968b29fc56ed7283cc4241e493b60fafb2d6
NakatoTeshika <aleksa_aleksa98@mail.ru>
2019-08-06 07:19:39 +0600

    Рефактор в CliParameterParser and HtmlParameterParser - удалены функции, создан конструктор

--- src/HtmlParameterParser.php
+++ src/HtmlParameterParser.php
@@ -6,36 +6,58 @@ use App\Abstracts\ParameterParser;
 
 class HtmlParameterParser extends ParameterParser
 {
+    protected $puppy_count = 0;
+
+    protected $kitty_count = 0;
+
+    protected $volumeBox = 0;
+
+    public function __construct()
+    {
+        if (isset($_GET['puppy_count'])) {
+            $this->puppy_count = $_GET['puppy_count'];
+        }
+        if (isset($_GET['kitty_count'])) {
+            $this->puppy_count = $_GET['kitty_count'];
+        }
+        if (isset($_GET['volume_box'])) {
+            $this->puppy_count = $_GET['volume_box'];
+        }
+    }
+
     /**
      * @return int
      * Количество щенят, значение введенное в браузерной строке
      */
-    public function getPuppyAmount(): int
+    /**
+     * @return int|mixed
+     */
+    public function getPuppyCount():int
     {
-        $puppy_count = $_GET['puppy_count'];
-
-        return (int)$puppy_count;
+        return $this->puppy_count;
     }
 
     /**
      * @return int
      * Количество котят, значение введенное в браузерной строке
      */
-    public function getKittyAmount(): int
+    /**
+     * @return int
+     */
+    public function getKittyCount(): int
     {
-        $kitty_count = $_GET['kitty_count'];
-
-        return  (int)$kitty_count;
+        return $this->kitty_count;
     }
 
     /**
      * @return int
      * Площадь коробки, значение введенное в браузерной строке
      */
+    /**
+     * @return int
+     */
     public function getBoxVolume(): int
     {
-        $volumeBox = $_GET['volume_box'];
-
-        return (int)$volumeBox;
+        return $this->volumeBox;
     }
 }
