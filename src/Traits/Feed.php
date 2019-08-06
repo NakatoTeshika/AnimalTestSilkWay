55e7dd718822267964bfaec8cc464bd66d5e49f9
NakatoTeshika <aleksa_aleksa98@mail.ru>
2019-08-06 07:08:48 +0600

    Рефактор в Feed - замена trait на class

--- src/Traits/Feed.php
+++ src/Traits/Feed.php
@@ -3,8 +3,6 @@
 namespace App\Traits;
 
 use App\Abstracts\Animal;
-use App\Abstracts\Максимальное;
-use App\Abstracts\Текущий;
 
 class Feed
 {
@@ -12,23 +10,10 @@ class Feed
      * @var
      * Текушая масса корма
      */
-    protected $weightOfFeed;
+    protected $weightOfFeed = 0;
 
     public function __construct($weightOfFeed)
     {
         $this->weightOfFeed = $weightOfFeed;
-    }
-
-
-
-    /**
-     * @param Animal $animal
-     * @return Максимальное|Текущий|int
-     * Масса максимально скормленного корма животным - вызывается в animalEat, используется в random - max
-     */
-    public function weight(Animal $animal)
-    {
-        $this->weightOfFeed = $animal->getMaxLevelFood()-$animal->getCurrentLevelFood();
-        return $this->weightOfFeed;
     }
 }
