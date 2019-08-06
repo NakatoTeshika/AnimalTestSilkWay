ab768c3da364ad0d58ea118b1d54174381ebc76b
NakatoTeshika <aleksa_aleksa98@mail.ru>
2019-08-06 07:28:12 +0600

    Рефактор в Application - добавлен вызов функций кормления и туалета

--- src/Application.php
+++ src/Application.php
@@ -8,6 +8,7 @@ use App\Patterns\BoxFactory;
 use App\Patterns\BoxPresenter;
 use App\Patterns\CatFactory;
 use App\Patterns\DogFactory;
+use App\Patterns\FoodFactory;
 use App\Patterns\OutOfBoxPresenter;
 
 class Application
@@ -23,18 +24,27 @@ class Application
      */
     public function run(View $view, ParameterParser $parameter)
     {
-        $box     = BoxFactory::create($parameter);
-        $dogs    = DogFactory::create($parameter);
-        $cats    = CatFactory::create($parameter);
-        $animals = array_merge($dogs, $cats);
-        shuffle($animals);
-        $boxPresenter = new BoxPresenter($box,$parameter);
-        $outOfBox = new OutOfBox();
+        $box               = BoxFactory::create($parameter->getBoxVolume());
+        $dogs              = DogFactory::create($parameter->getPuppyCount());
+        $cats              = CatFactory::create($parameter->getKittyCount());
+        $feeds             = FoodFactory::create($parameter->getPuppyCount()+$parameter->getKittyCount());
+        $animals           = array_merge($dogs, $cats);
+        $boxPresenter      = new BoxPresenter($box,$parameter);
+        $outOfBox          = new OutOfBox();
         $outOfBoxPresenter = new OutOfBoxPresenter($outOfBox);
-        $insert = new Insert();
-        $insert->insertInto($box,$outOfBox,$animals,$parameter);
-        $box->animalEatIn();
-        $outOfBox->animalEat();
+        $insert            = new AddAnimal();
+
+        shuffle($animals);
+        $insert->addToSomewhere($box,$outOfBox,$animals);
+
+        foreach ($animals as $animal) {
+            $animal->animalEat(array_pop($feeds));
+        }
+
+        $box->animalToilet();
+        $outOfBox->animalToilet();
+        $box->animalToiletIn();
         $view->view($boxPresenter, $outOfBoxPresenter);
+        $box->clear();
     }
 }
