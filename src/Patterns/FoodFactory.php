276400b296d1f612295ac534677a13e050ba6012
NakatoTeshika <aleksa_aleksa98@mail.ru>
2019-08-06 07:10:33 +0600

    Рефактор в FoodFactory - добавлена функция для создания корма

--- src/Patterns/FoodFactory.php
+++ src/Patterns/FoodFactory.php
@@ -4,7 +4,26 @@
 namespace App\Patterns;
 
 
-class FoodFactory
-{
+use App\Interfaces\IFactory;
+use App\Traits\Feed;
 
+class FoodFactory implements IFactory
+{
+    /**
+     * @param $parameter
+     * @return mixed
+     * Создание порции корма
+     */
+    static public function create($parameter)
+    {
+        $feed = [];
+        while($parameter>0)
+        {
+            $weightOfFeed = 150;
+            array_push($feed, new Feed($weightOfFeed));
+            $parameter--;
+        }
+
+        return $feed;
+    }
 }
