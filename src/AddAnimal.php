cabf75d4a3a1cee6e5ca807207421134834723c1
NakatoTeshika <aleksa_aleksa98@mail.ru>
2019-08-07 11:34:59 +0600

    Рефактор в AddAnimal - переименована функция на addTo

--- src/AddAnimal.php
+++ src/AddAnimal.php
@@ -2,19 +2,19 @@
 
 namespace App;
 
-
 class AddAnimal
 {
     /**
+     * Добавление зверюшки на улицу(в массив вне коробки, в случае если не добавили зверюшку в коробку)
      * @param Box $box
      * @param OutOfBox $outOfBox
      * @param array $animals
      */
-    public function addToSomewhere(Box $box, OutOfBox $outOfBox, array $animals)
+    public function addTo(Box $box, OutOfBox $outOfBox, array $animals)
     {
         foreach ($animals as $animal) {
             if (!$box->addAnimal($animal)) {
-                $outOfBox->addOut($animal);
+                $outOfBox->add($animal);
             }
         }
     }
