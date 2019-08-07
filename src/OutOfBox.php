a43bcf7711f705857d5ad29095443d9e7d789d27
NakatoTeshika <aleksa_aleksa98@mail.ru>
2019-08-07 11:45:55 +0600

    Рефактор в OutOfBox - изменено имя метода addOut

--- src/OutOfBox.php
+++ src/OutOfBox.php
@@ -3,74 +3,101 @@
 namespace App;
 
 use App\Abstracts\Animal;
-use App\Abstracts\BoxOrNot;
-use App\Interfaces\ICounter;
 
-class OutOfBox implements ICounter
+class OutOfBox
 {
     /**
-     * @var array Массив животных вне коробки
+     * Массив животных вне коробки
+     * @var array
      */
     public $storageOfPetOut  = array();
 
     /**
-     * @var array Массив для хранения экскременов
+     * Массив для хранения экскременов
+     * @var array
      */
     protected $wasteOutOfBox = array();
+
     /**
+     * Добавление в массив объектов, которые не поместились в коробку
      * @param Animal $animal
      * @return bool
-     * Добавление в массив объектов, которые не поместились в коробку
      */
-    public function addOut(Animal $animal)
+    public function add(Animal $animal)
     {
         $animal->setInBox(false);
         array_push($this->storageOfPetOut,$animal);
+
         return true;
     }
 
     /**
-     * @return array
-     * Количество животных вне коробки
+     * Количество кошек вне коробки
+     * @return int
      */
-    public function countIsAdd()
+    public function catsNotAdd():int
     {
         $notAddedCat = 0;
-        $notAddedDog = 0;
 
-        foreach ($this->storageOfPetOut as $animal)
-        {
-            if (is_a($animal, Dog::class)) {
-                $notAddedDog++;
-            }
+        foreach ($this->storageOfPetOut as $animal) {
             if(is_a($animal,Cat::class)) {
                 $notAddedCat++;
             }
         }
-
-        return ['notAddedDog'=>$notAddedDog, 'notAddedCat'=>$notAddedCat];
+        return $notAddedCat;
     }
 
     /**
-     * @return array
-     * Количество сытых и голодных животных вне коробки
+     * Количество собак вне коробки
+     * @return int
+     */
+    public function dogsNotAdd()
+    {
+        $notAddedDog = 0;
+
+        foreach ($this->storageOfPetOut as $animal) {
+            if(is_a($animal,Dog::class)) {
+                $notAddedDog++;
+            }
+        }
+        return $notAddedDog;
+    }
+
+    /**
+     * Количество голодных животных вне коробки
+     * @return int
+     */
+    public function countIsHungry():int
+    {
+        $countHungry = 0;
+
+        foreach ($this->storageOfPetOut as $animal) {
+            if ($animal->isHungry() == true) {
+                $countHungry++;
+            }
+        }
+        return $countHungry;
+    }
+
+    /**
+     * Количество сытых животных вне коробки
+     * @return int
      */
     public function countIsNotHungry()
     {
         $countNotHungryOut = 0;
-        $countHungryOut    = 0;
 
         foreach ($this->storageOfPetOut as $animal) {
             if ($animal->isHungry() == false) {
                 $countNotHungryOut++;
-            } elseif ($animal->isHungry() == true) {
-                $countHungryOut++;
             }
         }
-
-        return ['countNotHungryOut'=>$countNotHungryOut, 'countHungryOut'=>$countHungryOut];
+        return $countNotHungryOut;
     }
 
+    /**
+     * Поход в туалет
+     */
     public function animalToilet()
     {
         foreach ($this->storageOfPetOut as $animal) {
