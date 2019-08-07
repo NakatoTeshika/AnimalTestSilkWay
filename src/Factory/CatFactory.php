5375f8703bca5b8cd23032b8d74817e3080ef161
NakatoTeshika <aleksa_aleksa98@mail.ru>
2019-08-07 11:48:06 +0600

    Рефактор в CatFactory, DogFactory - операция разбита на отдельные этапы

--- src/Factory/CatFactory.php
+++ src/Factory/CatFactory.php
@@ -1,34 +1,37 @@
 <?php
 
-namespace App\Patterns;
+namespace App\Factory;
 
-use App\Abstracts\ParameterParser;
 use App\Cat;
 use App\Interfaces\IFactory;
 
 class CatFactory implements IFactory
 {
     /**
+     * Создание кошек, по заданному количеству
      * @param $parameter
      * @return array
-     *Создание кошек, по заданному количеству
      */
     static public function create($parameter)
     {
-        $cats         = array();
-        $name         = ["Пушок","Белка","Синька","Зеленка","Несси","Васька","Том","Джерри","Скотт"];
-        $species      = ["Мейн кун", "Сфинкс", "Британская", "Шотландская", "Бирманская", "Бурма","Норвежская"];
-        $genders      = ["м","ж"];
-        $animalColour = ["Белый",'черный','серый','рыжий','коричневый', 'пыльно-серый'];
-        $stomach      = array();
-        $limitKitty   = $parameter;
-        while($limitKitty>0)
-        {
-            array_push($cats, new Cat($name[array_rand($name,1)], $species[array_rand($species, 1)],
-                                              $genders[array_rand($genders, 1)], $animalColour[array_rand($animalColour, 1)],
-                                              rand(150, 200), rand(1, 12), rand(250, 500), $stomach));
-            $limitKitty--;
+        $cats            = array();
+        $name            = ["Пушок","Белка","Синька","Зеленка","Несси","Васька","Том","Джерри","Скотт"];
+        $species         = ["Мейн кун", "Сфинкс", "Британская", "Шотландская", "Бирманская", "Бурма","Норвежская"];
+        $genders         = ["м","ж"];
+        $animalColour    = ["Белый",'черный','серый','рыжий','коричневый', 'пыльно-серый'];
+        $limitKitty      = $parameter;
+        $catName         = $name[array_rand($name,1)];
+        $catSpecies      = $species[array_rand($species, 1)];
+        $catGender       = $genders[array_rand($genders, 1)];
+        $catColour       = $animalColour[array_rand($animalColour, 1)];
+        $catMaxLevelFood = rand(150, 200);
+        $catAge          = rand(1, 12);
+        $catVolume       = rand(250, 500);
 
+        while ($limitKitty>0) {
+            array_push($cats, new Cat($catName, $catSpecies, $catGender, $catColour,
+                                              $catMaxLevelFood, $catAge, $catVolume));
+            $limitKitty--;
         }
         return $cats;
     }
--- src/Factory/DogFactory.php
+++ src/Factory/DogFactory.php
@@ -1,31 +1,36 @@
 <?php
 
-namespace App\Patterns;
+namespace App\Factory;
 
-use App\Abstracts\ParameterParser;
 use App\Dog;
 use App\Interfaces\IFactory;
 
 class DogFactory implements IFactory
 {
     /**
+     * Создание щенят, по заданному количеству
      * @param $parameter
      * @return array|mixed
-     * Создание щенят, по заданному количеству
      */
     static public function create($parameter)
     {
-        $dogs         = array();
-        $name         = ["Тайсон", "Цезарь", "Джек", "Умка", "Робин", "Шарик", "Жучка", "Барон","Дружок"];
-        $species      = ["Шпиц", "Пудель","Лайка","Чихуахуа", "Терьер", "Мопс","Лабрадор","Овчарка","Бульдог","Такса"];
-        $genders      = ["ж","м"];
-        $animalColour = ["белый", "черный", "серый", "рыжий"];
-        $stomach      = array();
-        $limitPuppy   = $parameter;
+        $dogs            = array();
+        $name            = ["Тайсон", "Цезарь", "Джек", "Умка", "Робин", "Шарик", "Жучка", "Барон","Дружок"];
+        $species         = ["Шпиц", "Пудель","Лайка","Чихуахуа", "Терьер", "Мопс","Лабрадор","Овчарка","Бульдог","Такса"];
+        $genders         = ["ж","м"];
+        $animalColour    = ["белый", "черный", "серый", "рыжий"];
+        $limitPuppy      = $parameter;
+        $dogName         = $name[array_rand($name, 1)];
+        $dogSpecies      = $species[array_rand($species,1)];
+        $dogGender       = $genders[array_rand($genders, 1)];
+        $dogColour       = $animalColour[array_rand($animalColour,1)];
+        $dogMaxLevelFood = rand(150,250);
+        $dogAge          = rand(1,15);
+        $dogVolume       = rand(500, 1000);
+
         while ($limitPuppy>0) {
-            array_push($dogs, new Dog($name[array_rand($name, 1)], $species[array_rand($species,1)],
-                                       $genders[array_rand($genders,1)], $animalColour[array_rand($animalColour,1)],
-                                       rand(150,250), rand(1,15), rand(500, 1000), $stomach));
+            array_push($dogs, new Dog($dogName ,  $dogSpecies , $dogGender, $dogColour,
+                                              $dogMaxLevelFood, $dogAge , $dogVolume));
             $limitPuppy--;
         }
         return $dogs;
